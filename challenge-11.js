79575ece80794b6ccda927002699a0b6bf60a51a
Decio Francis <francisdasilva@gmail.com>
2019-02-11 17:59:57 -0300

    respotas challenge 11

--- challenge-11.js
+++ challenge-11.js
@@ -48,7 +48,12 @@
 	Após a função, mostrar a mensagem no console:
 	'The person has more than 25 years old? [TRUE/FALSE]'
 	*/
-	// ?
+	function moreThan(ageprop) {
+	    if(person.age > ageprop) {
+	      return true;
+	    }return false;
+	};
+	console.log('The person has more than 25 years old?', moreThan(25));
 
 	/*
 	Faça um loop de 0 a 20, que adicione cada número como um item de um
@@ -56,7 +61,14 @@
 	Mostre no console os números no array.
 	*/
 	console.log( 'De 0 a 10:' );
-	// ?
+	var numbers = [];
+	for(var i=0; i < 20; i++) {
+	  if(i > 10) {
+	    break;
+	  }
+	  numbers.push(i);
+	};
+	console.log(numbers);
 
 	/*
 	Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
@@ -65,5 +77,12 @@
 	Mostrar no console os números do array.
 	*/
 	console.log( 'Pares de 0 a 20:' );
-	// ?
+	var numbers = [];
+	for(var i=0; i <= 20; i++) {
+	  if(i % 2 !== 0) {
+	    continue;
+	  }
+	  numbers.push(i);
+	};
+	console.log(numbers);
 })();
